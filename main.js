14c513b64127b1876892d6dad3e32b176d442828
iAke <ekasitk.eng@gmail.com>
2021-08-10 22:53:11 +0700

    test

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const app = Vue.createApp({
             inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 },
             ]
         }
     },
@@ -19,6 +19,11 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        }
+    },
+    computed:{
+        title(){
+            return this.product + ' ' + this.brand
         }
     }
 })
